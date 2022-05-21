a26a83fc28511dce4e06e0c0c65e2ea3b8cd312e
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-05-21 20:47:01 +0600

    available

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { query } = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,12 +24,64 @@ async function run() {
     const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
+    const bookingCollection = client.db("doctors_portal").collection("booking");
 
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      //step 1: get all services
+      const services = await serviceCollection.find().toArray();
+
+      //step 2:get the booking of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      //step 3: for each service
+      services.forEach((service) => {
+        //step 4:  find booking for that service
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        service.available = available;
+      });
+
+      res.send(services);
+    });
+
+    /**
+     * API Naming Convention
+     *
+     * app.get('/booking') //get all the bookings in this collection or get more then one or by filter
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') //add new booking
+     * app.patch('/booking/:id') // update a specific booking
+     * app.delete('/booking/:id') // delete a specific booking
+     */
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
     });
   } finally {
   }

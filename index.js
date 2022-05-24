56fba09b71d2ff52facf63ec1b8a7b691c35aae4
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-05-24 13:58:57 +0600

    unauthorized added

--- index.js
+++ index.js
@@ -56,6 +56,30 @@ async function run() {
       res.send(users);
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccout = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccout.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.send(403).send({ message: "forbidden" });
+      }
+    });
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
